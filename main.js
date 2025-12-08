f922c9c7bbd26f40b0b37eea9501332ddb5f1ddd
shrub47381 <whatsapp37890@gmail.com>
2025-12-08 18:53:45 +0000

    now it should work..?

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function gainNumber(lowestGain){
 function mainLoop(){
     diff = (Date.now()-data.time)/1000
     data.time = Date.now()
+    let gain = D(1)
     gainNumber(gain.times(diff))
 window.setInterval(function(){
     mainLoop()
