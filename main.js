2bbffe15c50a737d233e5046ba278cca27b9a6d6
shrub47381 <whatsapp37890@gmail.com>
2025-12-09 16:56:00 +0000

    thing

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+let gain = data.numbers[1]
 function gainNumber(lowestGain){
     data.numbers[0] = data.numbers[0].plus(lowestGain)
 function mainLoop(){
     diff = (Date.now()-data.time)/1000
     data.time = Date.now()
-    let gain = D(1)
     gainNumber(gain.times(diff))
 window.setInterval(function(){
     mainLoop()
